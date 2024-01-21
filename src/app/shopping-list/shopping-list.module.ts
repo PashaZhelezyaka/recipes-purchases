f525364c710f8b6cd66057c51e5b486266e1b064
PashaZhelezyaka <pilipenkopv1991@gmail.com>
2024-01-21 13:50:29 +0300

    module optimization

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ShoppingListComponent } from "./shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { FormsModule } from "@angular/forms";
